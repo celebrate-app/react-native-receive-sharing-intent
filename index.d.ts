a33f8542808712ee2bd2c42567a522611b13cc46
Lucija Tatarević <lucija.tatarevic@gmail.com>
2023-07-13 12:34:19 +0200

    Update to useSharingIntent hook

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,24 @@
 declare namespace ReceiveSharingIntent {
-    export interface ShareIntentFile {
-        filePath: string|null;
-        text: string|null;
-        weblink: string|null;
-        mimeType: string|null;
-        contentUri: string|null;
-        fileName: string|null;
-        extension: string|null;
-        extra: any;
-    }
-    
-    export type ShareIntentResponse = ShareIntentFile[];
+  export interface ShareIntentFile {
+    filePath: string | null;
+    text: string | null;
+    weblink: string | null;
+    mimeType: string | null;
+    contentUri: string | null;
+    fileName: string | null;
+    extension: string | null;
+    extra: any;
+  }
 
-    export function getReceivedFiles(
-        callback: (files: ShareIntentResponse) => void,
-        errorHandler: (error: Error) => void,
-        protocol?: string
-    ): void;
+  export type ShareIntentResponse = ShareIntentFile[];
 
-    export function clearReceivedFiles(): void;
+  export function useSharingIntent(
+    handler: Function,
+    errorHandler: Function,
+    protocol?: string
+  ): void;
+
+  export function clearReceivedFiles(): void;
 }
 
 export = ReceiveSharingIntent;
--- src/utils.ts
+++ src/utils.ts
@@ -1,68 +1,79 @@
-import type { IReturnData, IUtils } from "./ReceiveSharingIntent.interfaces";
-import MimeTypes from './mimeTypes';
+import MimeTypes from 'mime-db';
 
-class Utils implements IUtils{
-
-    sortData(data: any): Array<IReturnData> {
-        const objects: IReturnData = { filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null };
-        const file = data;
-        if (file.startsWith('text:')) {
-            const text = file.replace("text:", "");
-            if (text.startsWith("http")) {
-                const object: Array<IReturnData> = [
-                    { ...objects, weblink: text }
-                ];
-                return object;
-            }
-            let object = [
-                { ...objects, text: text }
-            ];
-            return object;
-        } else if (file.startsWith('webUrl:')) {
-            const weblink: string = file.replace("webUrl:", "");
-            const object: Array<IReturnData> = [
-                { ...objects, weblink: weblink }
-            ];
-            return object;
-        } else {
-            try {
-                const files = JSON.parse(file)
-                const object = [];
-                for (let i = 0; i < files.length; i++) {
-                    const path = files[i].path;
-                    const obj = {
-                        ...objects,
-                        fileName: this.getFileName(path),
-                        extension: this.getExtension(path),
-                        mimeType: this.getMimeType(path),
-                        filePath: path,
-                        extra: files[i].extra
-                    }
-                    object.push(obj);
-                }
-                return object;
-            } catch (error) {
-                return [{ ...objects }];
-            }
-        }
-    }
-
-    getFileName = (file:string): string => {
-        return file.replace(/^.*(\\|\/|\:)/, '');
-    };
-
-    getExtension = (fileName:string): string => {
-        return fileName.substr(fileName.lastIndexOf('.') + 1);
-    }
-
-    getMimeType = (file:string): string => {
-        const ext = this.getExtension(file);
-        const extension = "." + ext.toLowerCase();
-        const type = Object.entries(MimeTypes).find(mime => mime[0] ===  extension)
-        if (type)return type[0];
-        return "";
-    }
-
+interface IReturnData {
+  filePath?: any | string;
+  text?: any | string;
+  weblink?: any | string;
+  mimeType?: any | string;
+  contentUri?: any | string;
+  fileName?: any | string;
+  extension?: any | string;
+  extra?: any;
 }
 
-export default Utils;
+const getFileName = (file: string): string => {
+  return file.replace(/^.*(\\|\/|:)/, '');
+};
+
+const getExtension = (fileName: string): string => {
+  return fileName.substring(fileName.lastIndexOf('.') + 1);
+};
+
+const getMimeType = (file: string): string => {
+  const ext = getExtension(file);
+  const extension = '.' + ext.toLowerCase();
+  const type = Object.entries(MimeTypes).find(
+    (mime) => mime[1]?.extensions && mime[1]?.extensions.includes(extension)
+  );
+
+  if (type) return type[0];
+  return '';
+};
+
+export function sortData(data: any): IReturnData[] {
+  const objects: IReturnData = {
+    filePath: null,
+    text: null,
+    weblink: null,
+    mimeType: null,
+    contentUri: null,
+    fileName: null,
+    extension: null,
+  };
+
+  const file = data;
+
+  if (file.startsWith('text:')) {
+    const text = file.replace('text:', '');
+    if (text.startsWith('http')) {
+      const object: IReturnData[] = [{ ...objects, weblink: text }];
+      return object;
+    }
+    const object = [{ ...objects, text }];
+    return object;
+  } else if (file.startsWith('webUrl:')) {
+    const weblink: string = file.replace('webUrl:', '');
+    const object: IReturnData[] = [{ ...objects, weblink }];
+    return object;
+  } else {
+    try {
+      const files = JSON.parse(file);
+      const object = [];
+      for (let i = 0; i < files.length; i++) {
+        const path = files[i].path;
+        const obj = {
+          ...objects,
+          fileName: getFileName(path),
+          extension: getExtension(path),
+          mimeType: getMimeType(path),
+          filePath: path,
+          extra: files[i].extra,
+        };
+        object.push(obj);
+      }
+      return object;
+    } catch (error) {
+      return [{ ...objects }];
+    }
+  }
+}
